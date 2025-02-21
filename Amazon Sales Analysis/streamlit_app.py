6ef7a4613435f212d70d67dc28191d5250c3e4b8
Gomolemo Kototsi <89483670+GomolemoKototsiAnalyst@users.noreply.github.com>
2025-02-21 10:16:36 +0000

    Update requirements.txt and streamlit_app.py

--- Amazon Sales Analysis/streamlit_app.py
+++ Amazon Sales Analysis/streamlit_app.py
@@ -718,7 +718,7 @@ with col[2]:
     st.markdown(
         """
         <style>
-        .scrollable-table {
+        .scrollable-table {streamlit run 
             max-height: 700px; /* Adjust height as needed */
             overflow-y: auto;
             overflow-x: hidden;
